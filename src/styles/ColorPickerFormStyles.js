8995a4dcca56b0fd269ea0ecba072771674ac355
NightmanCZ90 <novak.jan90@gmail.com>
2020-11-08 21:03:24 +0100

    creating seperate repository for this react project

---
+++ src/styles/ColorPickerFormStyles.js
@@ -0,0 +1,20 @@
+const styles = {
+  picker: {
+    marginTop: '2rem'
+  },
+  addColor: {
+    width: '100%',
+    padding: '1rem',
+    marginTop: '1rem',
+    fontSize: '2rem'
+  },
+  colorNameInput: {
+    width: '100%',
+    height: '70px',
+  },
+  spWidth: {  /////////////// without this it did not work properly
+    width: '100%'
+  }
+};
+
+export default styles;
